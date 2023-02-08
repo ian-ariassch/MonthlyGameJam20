1982d87dcbc725448c7aea54d56ba4fbbd587fca
Ian Arias Schreiber <ian.ariasschreiber@utec.edu.pe>
2023-02-08 10:18:19 -0300

    feat: added camera obstacle

---
+++ Assets/Scripts/CameraLight.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLight : MonoBehaviour
+{
+    private bool shouldWait = false;
+
+    [SerializeField] GameController _gameController;
+    [SerializeField] float speed = 1f;
+    [SerializeField] float waitTime = 2f;
+    [SerializeField] bool goingRight = true;
+
+    void Update()
+    {
+        if(shouldWait == false)
+        {
+            if (!goingRight)
+            {
+                transform.Translate(Vector3.up * speed * Time.deltaTime);
+            }
+            else
+            {
+                transform.Translate(Vector3.down * speed * Time.deltaTime);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            _gameController.GameOver();
+        }
+
+        Debug.Log("HIT");
+
+        if (other.gameObject.tag == "Wall")
+        {
+            goingRight = !goingRight;
+
+            Debug.Log("Wall hit");
+
+            StartCoroutine("Wait");
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        shouldWait = true;
+
+        yield return new WaitForSeconds(waitTime);
+
+        shouldWait = false;
+    }
+}
