d8ebf81c9d5b1471de54e7a39281ffaf613ce486
Ian Arias Schreiber <ian.ariasschreiber@utec.edu.pe>
2023-03-13 15:11:20 -0300

    conflicts

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -8,15 +8,32 @@ public class MainMenuController : MonoBehaviour
 
    [SerializeField] private GameObject _levelSelect;
 
+   [SerializeField] private GameObject _howToPlay;
+
    public void loadLevelSelect()
    {
         _mainMenu.SetActive(false);
+
+        _howToPlay.SetActive(false);
+
         _levelSelect.SetActive(true);
    }
 
    public void loadMainMenu()
    {
          _mainMenu.SetActive(true);
+
+         _howToPlay.SetActive(false);
+
+         _levelSelect.SetActive(false);
+   }
+
+   public void loadHowToPlay()
+   {
+         _mainMenu.SetActive(false);
+   
+         _howToPlay.SetActive(true);
+   
          _levelSelect.SetActive(false);
    }
 
